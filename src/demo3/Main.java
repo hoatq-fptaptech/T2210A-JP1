fe7f918884e7d0a1247c8ee638688f2f207b9795
macbook <quanghoa.trinh@gmail.com>
2023-05-06 15:57:46 +0700

    ke thua - da hinh

--- src/demo3/Main.java
+++ src/demo3/Main.java
@@ -1,5 +1,7 @@
 package demo3;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
         Student s = new Student(1,"Nam",18);
@@ -7,6 +9,17 @@ public class Main {
         FPTStudent f = new FPTStudent(2,"Minh",19,"0987612345");
         f.learn();
         f.playFootball();
-
+        ArrayList arrList =  new ArrayList();
+        arrList.add(100);
+        arrList.add("hello");
+        arrList.remove("hello");
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("world");
+       // stringArrayList.remove(0);// vị trí cần xóa
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        System.out.println(stringArrayList.get(0));// arr[0]
+        for(String st: stringArrayList){
+            //st <=> stringArrayList.get(i)
+        }
     }
 }
