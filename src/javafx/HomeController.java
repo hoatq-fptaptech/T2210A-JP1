8e22c3a2081d5c33fce409905f6b8bc4db16412a
macbook <quanghoa.trinh@gmail.com>
2023-05-16 17:04:27 +0700

    demo jv fx

--- src/javafx/HomeController.java
+++ src/javafx/HomeController.java
@@ -1,8 +1,11 @@
 package javafx;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class HomeController {
 
@@ -11,10 +14,26 @@ public class HomeController {
     public TextField txtTel;
     public Text txtOut;
 
+    public ArrayList<String> emailList= new ArrayList<>();
+    public ArrayList<String> tellList= new ArrayList<>();
+
     public void submit(ActionEvent actionEvent) {
-        String name = txtName.getText();
-        String email = txtEmail.getText();
-        String tel = txtTel.getText();
-        txtOut.setText(name+"\n"+email+"\n"+tel);
+        try {
+            String name = txtName.getText();
+            String email = txtEmail.getText();
+            String tel = txtTel.getText();
+            if(emailList.contains(email))
+                throw new Exception("Email đã tồn tại");
+            if(tellList.contains(tel))
+                throw new Exception("Tel đã tồn tại");
+            emailList.add(email);
+            tellList.add(tel);
+            String sv = "\n"+name+"\n"+email+"\n"+tel;
+            txtOut.setText(txtOut.getText()+sv);
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText(e.getMessage());
+            alert.show();
+        }
     }
 }
