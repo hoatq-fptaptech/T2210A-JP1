457549eba3d7523506f5a37ab2481b07a985f1b1
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-05-30 16:05:44 +0700

    show student to listview

--- src/javafx/HomeController.java
+++ src/javafx/HomeController.java
@@ -15,6 +15,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -24,6 +28,10 @@ public class HomeController implements Initializable {
     public ListView<Student> lv;
     public static Student editStudent;
 
+    private final static String connectionString = "jdbc:mysql://localhost:3306/t2210a_jp";
+    private final static String user = "root";
+    private final static String pwd = "root";// xampp: ""  mamp: "root"
+
     public void goToForm(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("form.fxml"));
         Main.mainStage.setScene(new Scene(root,600,400));
@@ -31,6 +39,28 @@ public class HomeController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        lv.setItems(listStudents);
+       // lv.setItems(listStudents);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(connectionString,user,pwd);
+
+            // query
+            Statement stt = conn.createStatement();
+            String sql = "select * from students";
+            ResultSet rs = stt.executeQuery(sql);
+            ObservableList<Student> list = FXCollections.observableArrayList();
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String tel = rs.getString("tel");
+                Student s = new Student(name,email,tel);
+                list.add(s);
+            }
+            lv.setItems(list);
+            lv.refresh();
+        }catch (Exception e){
+            System.out.println("error:"+e.getMessage());
+        }
     }
 }
